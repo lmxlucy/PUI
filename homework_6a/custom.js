54aeb9ced678ef438a34426eb344322c52d91986
Lucy <meixuanl@andrew.cmu.edu>
2021-10-15 12:30:54 -0400

    open shopping cart as a popup

--- homework_6a/custom.js
+++ homework_6a/custom.js
@@ -8,11 +8,3 @@ function showGlazingChange() {
     var selected_glazing = document.getElementById("glazing").value;
     document.getElementById("glazing_chosen").innerText = selected_glazing;
 }
-
-function openCart() {
-    document.getElementById("overlay").style.display = "block";
-}
-
-function closeCart() {
-    document.getElementById("overlay").style.display = "none";
-}
